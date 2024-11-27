ca8c9e199ba6a3d3fb0b8c73d010ad8311ab7b3b
HTRMC <htrmcox@outlook.com>
2024-11-27 13:21:26 +0100

    Update gitignore

--- ChristmasCard/ChristmasCard/obj/Debug/net8.0/ChristmasCard.AssemblyInfo.cs
+++ ChristmasCard/ChristmasCard/obj/Debug/net8.0/ChristmasCard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChristmasCard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+351062d80d76a46ee03be6185c28c3c9c56f71b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2837fdb0185648d732219794e50a6ce96fabbb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChristmasCard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChristmasCard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
